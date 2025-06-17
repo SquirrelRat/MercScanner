bff5e0aa3fe54eed692c3ebecc4130e1176249e6
instant.sc <code@insc.cc>
2025-06-17 22:23:59 +0200

    Add project files.

---
+++ MercScanner.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Linq;
+using ExileCore;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
+using ExileCore.Shared.Enums;
+using ExileCore.Shared.Helpers;
+using ImGuiNET;
+using SharpDX;
+using Color = System.Drawing.Color;
+using Vector2 = System.Numerics.Vector2;
+
+namespace MercScanner;
+
+public class MercScanner : BaseSettingsPlugin<MercScannerSettings>
+{
+    public override bool Initialise()
+    {
+        return true;
+    }
+
+    public override Job Tick()
+    {
+        return null;
+    }
+
+    public override void Render()
+    {
+        if (!Settings.IgnoreLargePanels && GameController.IngameState.IngameUi.LargePanels.Any(x => x.IsVisible) ||
+            !Settings.IgnoreFullscreenPanels && GameController.IngameState.IngameUi.FullscreenPanels.Any(x => x.IsVisible))
+        {
+            return;
+        }
+
+        foreach (var idleMerc in GameController.EntityListWrapper.ValidEntitiesByType[EntityType.Monster]
+                     .Where(x => x.Metadata.StartsWith("Metadata/Monsters/Mercenaries/", StringComparison.Ordinal) &&
+                                 !x.IsHostile &&
+                                 x.TryGetComponent<Positioned>(out var positioned) &&
+                                 positioned.Reaction == 70))
+        {
+            if (idleMerc.TryGetComponent<Actor>(out var actor))
+            {
+                var line = 0;
+                var merScreenPos = GameController.IngameState.Camera.WorldToScreen(idleMerc.PosNum);
+                var lineHeight = ImGui.GetTextLineHeight();
+                foreach (var skill in actor.ActorSkills.Where(x => !string.IsNullOrWhiteSpace(x.Name)))
+                {
+                    var fullSkillName = skill.Name;
+                    if (fullSkillName == "Move" || fullSkillName == "EASMercenaryPortalOut")
+                    {
+                        continue;
+                    }
+
+                    var skillName = fullSkillName;
+                    if (skillName.EndsWith("Mercenary"))
+                    {
+                        skillName = skillName[..^"Mercenary".Length];
+                    }
+
+                    var isHighlighted = Settings.Auras.Content.Any(x => !string.IsNullOrWhiteSpace(x.Value) &&
+                                                                        (skillName.Contains(x.Value, StringComparison.InvariantCultureIgnoreCase) ||
+                                                                         fullSkillName.Contains(x.Value, StringComparison.InvariantCultureIgnoreCase)));
+                    if (!isHighlighted && !Settings.ShowAllSkills)
+                    {
+                        continue;
+                    }
+
+                    Graphics.DrawTextWithBackground(skillName,
+                        merScreenPos + new Vector2(0, lineHeight * line),
+                        isHighlighted
+                            ? Settings.HighlightSkillColor
+                            : Settings.DefaultSkillColor, Settings.BackgroundColor);
+                    line++;
+                }
+            }
+        }
+    }
+}
---
+++ MercScannerSettings.cs
@@ -0,0 +1,19 @@
+﻿using ExileCore.Shared.Helpers;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using SharpDX;
+using Color = System.Drawing.Color;
+
+namespace MercScanner;
+
+public class MercScannerSettings : ISettings
+{
+    public ToggleNode Enable { get; set; } = new ToggleNode(false);
+    public ToggleNode IgnoreLargePanels { get; set; } = new ToggleNode(false);
+    public ToggleNode IgnoreFullscreenPanels { get; set; } = new ToggleNode(false);
+    public ColorNode HighlightSkillColor { get; set; } = new ColorNode(Color.Green.ToSharpDx());
+    public ColorNode DefaultSkillColor { get; set; } = new ColorNode(Color.White.ToSharpDx());
+    public ColorNode BackgroundColor { get; set; } = new ColorNode(Color.Black.ToSharpDx());
+    public ToggleNode ShowAllSkills { get; set; } = new ToggleNode(true);
+    public ContentNode<TextNode> Auras { get; set; } = new ContentNode<TextNode>() { EnableControls = true, UseFlatItems = true, ItemFactory = () => new TextNode("") };
+}
